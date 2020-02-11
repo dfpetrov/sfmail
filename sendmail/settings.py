1e86ef74ad8aede99463159fe789a2c7846fa35f
Гараев Денис Флюрович <denis.garaev@OCRV.COM.RZD>
2020-02-11 14:20:27 +0300

    Init

--- sendmail/settings.py
+++ sendmail/settings.py
@@ -126,5 +126,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.mail.ru'
 EMAIL_PORT = 2525
 EMAIL_HOST_USER = "dfpetrov@mail.ru"
-EMAIL_HOST_PASSWORD = "xbrbgebrb"
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 EMAIL_USE_TLS = True
